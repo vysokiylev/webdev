3f8f985883797b35f79596a2d819fef06be14fde
Lev Vysokiy <visoko96.lev@mail.ru>
2017-11-19 12:38:04 +0500

    lab 1

---
+++ lab1/nettech_lab/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace nettech_lab.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Demo() {
+            //ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+    }
+}
